940fb515904d6c017e665e2c2ae5a282f26e94eb
TiffL <lei.tiffany@gmail.com>
2016-03-06 21:18:41 -0500

    basic outline

---
+++ brickbreaker.js
@@ -0,0 +1,8 @@
+var vimg = document.getElementById("svgimg");
+
+var startGame = function(){
+
+};
+
+var startButton = document.getElementById("start");
+startButton.addEventListener("click",startGame);
